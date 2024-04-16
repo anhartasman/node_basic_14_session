a8708b8d5abd1b11828a6ae4f4b03fec7e3f0e2f
Anhar <anhar_tasman@yahoo.com>
2024-04-17 02:38:51 +0700

    Setting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,6 @@ exports.getLogin = (req, res, next) => {
   });
 };
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  req.setHeader("Set-Cookie", "loggedIn=true");
   res.redirect("/");
 };
